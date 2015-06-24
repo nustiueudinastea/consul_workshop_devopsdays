07d6195acf0a0b9d59ae9800f051aebabe891756
Alex Giurgiu <alex.giurgiu@gmail.com>
2015-06-24 12:12:54 +0200

    master secondary election

--- code/cluster_manager/cluster_manager.py
+++ code/cluster_manager/cluster_manager.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2.7
 
 from consul import Check, Consul
 import logging
@@ -6,6 +7,7 @@ import time
 import sys
 
 NAME = 'ClusterManager'
+STATUS_PASSING = 'passing'
 
 sh = logging.StreamHandler(sys.stdout)
 logger = logging.getLogger(NAME)
@@ -17,24 +19,55 @@ class ClusterManager:
         self.name = NAME
         self.id = self.name + str(id)
         self.ttl = ttl
+        self.check_id = 'service:' + self.id
+        self.master = None
         self.client = Consul(host='172.17.42.1')
 
     def register(self):
         logger.info('Registering service {} with id {}'.format(self.name, self.id))
         self.client.agent.service.register(name=self.name, service_id=self.id, check=Check.ttl('10s'))
+        self.client.agent.check.ttl_pass(check_id=self.check_id)
+        self.session = self.client.session.create(behavior='delete', checks=[self.check_id])
 
     def deregister(self):
         logger.info('Deregistering service {}'.format(self.id))
         self.client.agent.service.deregister(service_id=self.id)
+        logger.info('Destroying session {}'.format(self.session))
+        self.client.session.destroy(self.session)
+
+    def set_mode(self):
+
+        logger.info('Trying to become master')
+        if self.client.kv.put('master', str(self.id), acquire=self.session):
+            self.master = None
+            logger.info('Became master')
+        else:
+            key = self.client.kv.get('master')
+            if key[1]:
+                self.master = key[1]['Value']
+                logger.info('Became secondary, master is {}'.format(self.master))
+            else:
+                logger.warning('Could not acquire master lock and no master available')
+
+    def check_master(self):
+        sub_services = self.client.health.service(self.name)[1]
+        sub_service = next((item for item in sub_services if item['Service']['ID'] == self.master), None)
+        if sub_service:
+            service_check = next((item for item in sub_service['Checks'] if item['CheckID'] == 'service:{}'.format(self.master)), None)
+            if service_check and service_check.get('Status', '') == STATUS_PASSING:
+                logger.info('Master {} is alive'.format(self.master))
+                return
+        self.set_mode()
 
     def pass_check(self):
-        check_id = 'service:' + self.id
-        logger.info('Starting service check loop for {}'.format(check_id))
+        logger.info('Starting service check loop for {}'.format(self.check_id))
         while True:
             try:
-                logger.debug('Updating service check ttl for {}'.format(check_id))
-                self.client.agent.check.ttl_pass(check_id=check_id)
-                time.sleep(8)
+                logger.debug('Updating service check ttl for {}'.format(self.check_id))
+                self.client.agent.check.ttl_pass(check_id=self.check_id)
+                if self.master:
+                    self.check_master()
+                time.sleep(5)
             except KeyboardInterrupt:
                 self.deregister()
                 sys.exit()
@@ -45,4 +78,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
     cm = ClusterManager(args.id, 10)
     cm.register()
+    cm.set_mode()
     cm.pass_check()
